8f1674f24025f5bbdd6dc9c88c5b20b89bccf992
ErSnDubey <ersndubey@gmail.com>
2023-05-31 20:49:13 +0530

    resolve confilct between voting buttons

--- script.js
+++ script.js
@@ -17,22 +17,33 @@ window.addEventListener('load', updateElementClass);
 
 
 
+// voting buttons 
+// Initialize an object to store vote counts for different posts
+const voteCounts = {};
 
-let voteCount = 0;
-
-function voteUp() {
-  voteCount++;
-  updateVoteCount();
+function voteUp(postId) {
+  if (!voteCounts.hasOwnProperty(postId)) {
+    voteCounts[postId] = 0;
+  }
+  voteCounts[postId]++;
+  updateVoteCount(postId);
 }
 
-function voteDown() {
-  voteCount--;
-  updateVoteCount();
+function voteDown(postId) {
+  if (!voteCounts.hasOwnProperty(postId)) {
+    voteCounts[postId] = 0;
+  }
+  voteCounts[postId]--;
+  updateVoteCount(postId);
 }
 
-function updateVoteCount() {
-  document.getElementById("voteCount").innerText = voteCount;
+function updateVoteCount(postId) {
+  const voteCountElement = document.getElementById(`voteCount_${postId}`);
+  if (voteCountElement) {
+    voteCountElement.innerText = voteCounts[postId] || 0;
+  }
 }
+
 
 
 
